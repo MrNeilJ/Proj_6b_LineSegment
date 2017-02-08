7eb2ccd5b63b350535407b471583fbd157010e5e
neiljohnson <mr.neil.j@gmail.com>
2017-02-08 15:52:38 -0800

    Added in necessary files for the classes needed to run the application

---
+++ LineSegment.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Neil Johnson on 2/8/17.
+//
+
+#ifndef LINESEGMENT_LINESEGMENT_HPP
+#define LINESEGMENT_LINESEGMENT_HPP
+
+
+class LineSegment {
+
+};
+
+
+#endif //LINESEGMENT_LINESEGMENT_HPP
